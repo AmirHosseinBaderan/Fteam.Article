5531da9e14ac572d6e2d3e26911ae587429d62ac
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-10-24 19:38:32 +0330

    Create IFileGet.cs

---
+++ Fteam.Article/Service/Fteam.Article.Services.Abstraction/File/IFileGet.cs
@@ -0,0 +1,10 @@
+﻿namespace Fteam.Article.Services.Abstraction;
+
+public interface IFileGet : IAsyncDisposable
+{
+    Task<IEnumerable<Attachment>> GetFilesAsync(Guid objectId, byte type);
+
+    Task<IEnumerable<Attachment>> GetFilesAsync(Guid objectId);
+
+    Task<Attachment?> FindFileAsync(Guid obejctId, byte type);
+}
