277c8296bb2b8dade7de41336f923c5c64d74ecb
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-10-24 19:37:57 +0330

    create usings

---
+++ Fteam.Article/Service/Fteam.Article.Services.Implemention/Usings.cs
@@ -0,0 +1,5 @@
+﻿global using Fteam.Article.DataBase.Context;
+global using Fteam.Article.DataBase.Entitiy;
+global using Fteam.Article.Services.Abstraction;
+global using Microsoft.EntityFrameworkCore;
+global using System.Linq.Expressions;
