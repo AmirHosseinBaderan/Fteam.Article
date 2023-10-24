b6f35250e371945866067ac2659c00b4d27435e4
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-10-24 14:24:53 +0330

    create project and build db context

---
+++ Fteam.Article/DataBase/Fteam.Article.DataBase.Context/Context/ArticleContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Fteam.Article.DataBase.Context;
+
+public class ArticleContext : DbContext
+{
+    public static string ConnectionString { get; set; }
+
+    public ArticleContext(DbContextOptions<ArticleContext> options) : base(options)
+    { }
+
+    public ArticleContext()
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+            optionsBuilder.UseSqlServer(ConnectionString);
+    }
+}
