4122f0aa7fffdabfc52a73f2f43c680bf083adc2
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-10-24 19:38:40 +0330

    create projectg

---
+++ Fteam.Article/Service/Fteam.Article.Services.Implemention/Cache/KeyGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace Fteam.Article.Services.Implementation;
+
+public static class KeyGenerator
+{
+    public static string StoreSettings(Guid storeId) => $"Store-Settings-{storeId}";
+
+    public static string ProductsViewModel(int page, int count, double lat, double lon, Guid? groupId) => $"products-view-model-{page}-{count}-{lat}-{lon}-{groupId}";
+
+    public static string StoreProductsViewModel(Guid storeId, int page, int count, Guid? groupId) => $"Store-Products-View-Model-{storeId}-{page}-{count}-{groupId}";
+}
