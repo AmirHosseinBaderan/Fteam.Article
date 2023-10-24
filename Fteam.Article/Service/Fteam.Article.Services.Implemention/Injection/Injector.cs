719e457ba2f33251c5bf7146a0cb404108e674d0
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-10-24 19:38:00 +0330

    Create Injector.cs

---
+++ Fteam.Article/Service/Fteam.Article.Services.Implemention/Injection/Injector.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Fteam.Article.Services.Implementation.Injection;
+
+public static class Injector
+{
+    public static IServiceCollection AddCoffeeBaseServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        #region -- Data Base SQL --
+
+        string connection = configuration.GetConnectionString("ArticleDb") ?? "";
+        ArticleContext.ConnectionString = connection;
+        services.AddDbContext<ArticleContext>(options => options.UseSqlServer(connection));
+
+        #endregion
+
+        #region -- Data Base No SQL --
+
+        //services.AddSingleton<IMongoClient>((service) =>
+        //{
+        //    string? connectionString = configuration.GetConnectionString("CoffeeNoSQL");
+        //    return new MongoClient(connectionString);
+        //});
+
+        #endregion
+
+        services.AddBaseServices();
+
+        services.AddBusinessServices();
+        services.AddExtranalServices();
+
+        return services;
+    }
+
+    static void AddBaseServices(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IBaseCud<>), typeof(BaseCud<>));
+        services.AddScoped(typeof(IBaseQuery<>), typeof(BaseQuery<>));
+
+    }
+
+    static void AddBusinessServices(this IServiceCollection services)
+    {
+        #region -- File --
+
+        services.AddTransient<IFileGet, FileGet>();
+        services.AddTransient<IFileAction, FileAction>();
+        services.AddTransient<IFileViewModel, FileViewModelService>();
+
+        #endregion
+
+    }
+
+    static void AddExtranalServices(this IServiceCollection services)
+    {
+        //services.AddFteamIdentityService();
+        //services.AddFteamLiveConnectionService();
+        //services.AddFteamNotificationService();
+    }
+}
